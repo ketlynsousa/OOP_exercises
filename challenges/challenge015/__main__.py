2e431ca4bf583fa3e816ca3f88c4bf14456e3468
Ketlyn Sousa <ketlynsousa3@gmail.com>
2026-08-28 12:17:02 -0300

    Improving Credential class

--- challenges/challenge015/__main__.py
+++ challenges/challenge015/__main__.py
@@ -4,10 +4,10 @@ from rich import inspect, print
 
 def main():
     c = Credential()
-    c.password = str(input('Enter password: '))
-    print(c.password)
 
-    c.validate(' Cricket@! ')
+    c.password = str(input('Enter password: '))
+    c.validate(str(input('Enter password again: ')))
 
 if __name__ == '__main__':
     main()
+
